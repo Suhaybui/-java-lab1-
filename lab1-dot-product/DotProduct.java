a0be4bf412c9542dcda5cbeb5175ec6181f34338
Suhaybui <114768604+Suhaybui@users.noreply.github.com>
2025-09-14 19:35:50 -0700

    Update DotProduct.java

--- lab1-dot-product/DotProduct.java
+++ lab1-dot-product/DotProduct.java
@@ -1,1 +1,23 @@
+import java.util.Arrays;
+import java.util.Random;
 
+public class DotProduct {
+    public static void main(String[] args) {
+        int n = 5; 
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int[] c = new int[n];
+
+        Random rng = new Random();
+
+        for (int i = 0; i < n; i++) {
+            a[i] = rng.nextInt(10);
+            b[i] = rng.nextInt(10);
+            c[i] = a[i] * b[i];
+        }
+
+        System.out.println("a = " + Arrays.toString(a));
+        System.out.println("b = " + Arrays.toString(b));
+        System.out.println("c = " + Arrays.toString(c));
+    }
+}
